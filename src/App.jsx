67a7d0a355a2759248f5368216331bc973316c96
Shehbaaz Aalam <shehbaazaalam00786@gmail.com>
2025-03-10 17:09:15 +0530

    weather-app completed !!!

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wheather from './components/Weather';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Wheather/>
+    </div>
+  )
+}
+
+export default App
